0f4ebaa52c13845b5ce9a1e9f65800ceaa8e94c9
Carlos Beltre <Carlos Beltre>
2016-09-25 23:49:31 -0400

    testing out different layouts and drag/drop functionality for a table/vbox

--- UMLEditor/src/Main.java
+++ UMLEditor/src/Main.java
@@ -4,127 +4,55 @@ import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Line;
-import javafx.scene.shape.Polygon;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
-import UMLEditor.Controller.Controller;
-
+//import UMLEditor.Controller.Controller;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseButton;
+import javafx.scene.layout.VBox;
+import javafx.geometry.Point2D;
+import javafx.scene.Cursor;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import UMLEditor.View.Classbox;
 import java.awt.*;
 
 public class Main extends Application {
-    Stage window;
-    Button button;
-
-    Circle circle_red, circle_green;
-    double orgSceneX, orgSceneY;
-    double orgTranslateX, orgTranslateY;
-    Point m_start = new Point(320, 20);
-    Point m_end = new Point(440, 20);
-
-    @Override
-    public void start(Stage primaryStage)
-    {
-
-//        window = primaryStage;
-//        window.setTitle("Test Window");
-//        button = new Button("Click Me!");
-//
-//        //Create Circles
-//        circle_red = new Circle(50.0f, Color.RED);
-//        circle_red.setCursor(Cursor.OPEN_HAND);
-//        circle_red.setCenterX(150);
-//        circle_red.setCenterY(150);
-//        circle_red.setOnMousePressed(circleOnMousePressedEventHandler);
-//        circle_red.setOnMouseDragged(circleOnMouseDraggedEventHandler);
-//
-//        circle_green = new Circle(50.0f, Color.GREEN);
-//        circle_green.setCursor(Cursor.OPEN_HAND);
-//        circle_green.setCenterX(200);
-//        circle_green.setCenterY(150);
-//        circle_green.setOnMousePressed(circleOnMousePressedEventHandler);
-//        circle_green.setOnMouseDragged(circleOnMouseDraggedEventHandler);
-//
-//        button.setAlignment(Pos.CENTER);
-//        button.setOnAction(e -> {
-//            boolean result = ConfirmBox.display();
-//            //System.out.println(result);
-//            if(result){
-//                circle_red.setFill(Color.GREEN);
-//            }else {
-//                circle_red.setFill(Color.BLUE);
-//            }
-//        });
-//
-//        Group layout = new Group();
-//        layout.getChildren().addAll(circle_red, circle_green, button);
-//
-//        Scene scene = new Scene(layout, 700, 650);
-//        window.setResizable(false);
-//        window.setScene(scene);
-//        window.show();
-
-        Group root = new Group();
-        Scene scene = new Scene(root, 400, 350);
-        primaryStage.setScene(scene);
-
-        Group g = new Group();
-
-        Line line = new Line();
-        line.setStartX(10.0);
-        line.setStartY(60.0);
-        line.setEndX(300.0);
-        line.setEndY(60.0);
-        line.setStrokeWidth(3.0);
-
-        Polygon polygon = new Polygon();
-        polygon.getPoints().addAll(new Double[]{
-                300.0, 60.0,
-                320.0, 45.0,
-                340.0, 60.0,
-                320.0, 75.0
-        });
-
-        g.getChildren().addAll(polygon, line);
-
-        scene.setRoot(g);
-        primaryStage.show();
-
-    }
-
-//    EventHandler<MouseEvent> circleOnMousePressedEventHandler =
-//            new EventHandler<MouseEvent>()
-//            {
-//                @Override
-//                public void handle(MouseEvent event)
-//                {
-//                    orgSceneX = event.getSceneX();
-//                    orgSceneY = event.getSceneY();
-//                    //Typecasting Circle to the event
-//                    orgTranslateX = ((Circle)(event.getSource())).getTranslateX();
-//                    orgTranslateY = ((Circle)(event.getSource())).getTranslateY();
-//                }
-//            };
-//
-//    EventHandler<MouseEvent> circleOnMouseDraggedEventHandler =
-//            new EventHandler<MouseEvent>()
-//            {
-//                @Override
-//                public void handle(MouseEvent event)
-//                {
-//                    double offsetX = event.getSceneX() - orgSceneX;
-//                    double offsetY = event.getSceneY() - orgSceneY;
-//                    double newTranslateX = orgTranslateX + offsetX;
-//                    double newTranslateY = orgTranslateY + offsetY;
-//
-//                    ( (Circle)(event.getSource()) ).setTranslateX(newTranslateX);
-//                    ( (Circle)(event.getSource()) ).setTranslateY(newTranslateY);
-//
-//                }
-//            };
-
 
     public static void main(String[] args) {
         launch(args);
     }
+
+    Stage window;
+    private Pane editPane = new Pane();
+    private TextArea className;
+    private TextArea classMethods;
+    private TextArea classFunctions;
+
+
+
+    @Override
+    public void start(Stage primaryStage) {
+        window = primaryStage;
+        window.setTitle("Test Window");
+
+        Classbox box = new Classbox();
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setCenter(box);
+
+        Scene scene = new Scene(borderPane, 600, 550);
+        window.setScene(scene);
+        window.show();
+
+    }
+
+
+
 }
--- UMLEditor/src/UMLEditor/View/Classbox.java
+++ UMLEditor/src/UMLEditor/View/Classbox.java
@@ -1,30 +1,97 @@
 package UMLEditor.View;
 
-import javafx.scene.control.ScrollPane;
 import java.util.ArrayList;
-import java.util.Observable;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.control.ContextMenu;
-import javafx.scene.control.MenuItem;
+
 import javafx.scene.control.TextArea;
-import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.geometry.Point2D;
-import javafx.scene.Cursor;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.effect.DropShadow;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.Priority;
-import javafx.geometry.HPos;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 /**
  * Created by beltre on 9/21/16.
  */
 public class Classbox extends VBox {
+
+    private double orgX, orgY;
+    private double height, width;
+    private Point2D[] anchorPoints; // array of each anchorpoint in the box
+    private Point2D dragAnchor; // new location of anchor after moved
+    private TextArea className;
+    private TextArea classMethods;
+    private TextArea classFunctions;
+
+
+    public Classbox() {
+        super();
+
+        width = 150.0;
+        height = 200.0;
+        anchorPoints = new Point2D[4];
+        setAnchorPoints(100.0, 100.0);
+
+        className = new TextArea();
+        className.setPromptText("Name");
+        className.setPrefRowCount(1);
+        className.setPrefColumnCount(10);
+        className.setWrapText(true);
+        className.setEditable(false);
+        className.setMouseTransparent(true);
+
+        classMethods = new TextArea();
+        classMethods.setPromptText("Methods");
+        classMethods.setPrefRowCount(2);
+        classMethods.setPrefColumnCount(10);
+        classMethods.setWrapText(true);
+        classMethods.setEditable(false);
+        classMethods.setMouseTransparent(true);
+        setVgrow(classMethods, Priority.ALWAYS);
+
+        classFunctions = new TextArea();
+        classFunctions.setPromptText("Functions");
+        classFunctions.setPrefRowCount(3);
+        classFunctions.setPrefColumnCount(10);
+        classFunctions.setWrapText(true);
+        classFunctions.setEditable(false);
+        classFunctions.setMouseTransparent(true);
+        setVgrow(classFunctions, Priority.ALWAYS);
+
+        getChildren().addAll(className, classMethods, classFunctions);
+        this.setMaxWidth(width);
+        this.setMaxHeight(height);
+        this.setStyle("-fx-border-style: solid;" + "-fx-border-width: 2;"
+                + "-fx-border-color: black;");
+
+    //Events
+        this.setOnMousePressed(e -> {
+            orgX = getTranslateX();
+            orgY = getTranslateY();
+            dragAnchor = new Point2D(e.getSceneX(), e.getSceneY());
+            e.consume();
+        });
+
+        this.setOnMouseDragged(e -> {
+            double dragX = e.getSceneX() - dragAnchor.getX();
+            double dragY = e.getSceneY() - dragAnchor.getY();
+            double newX = orgX + dragX;
+            double newY = orgY + dragY;
+
+        });
+
+
+
+    }
+
+    public void setAnchorPoints(double x, double y){
+        anchorPoints[0] = new Point2D(x, y + (height/2)); //left
+        anchorPoints[1] = new Point2D(x + (width/2), y); //top
+        anchorPoints[2] = new Point2D(x + width, y + (height/2)); //right
+        anchorPoints[3] = new Point2D(x + (width/2), y + height); //bottom
+    }
+
 
 }
