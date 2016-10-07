03358cee48c388dafb646e29eadf4f101fb1d551
Carlos Beltre <Carlos Beltre>
2016-10-06 23:43:28 -0400

    added phils stuff

---
+++ src/editor/Menu.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package editor;
+
+import javafx.scene.control.SeparatorMenuItem;
+
+/**
+ *
+ * @author pslandis
+ */
+public class Menu {
+    // parent instance
+    private final Main main;
+    
+    public javafx.scene.control.MenuBar bar;
+    
+    public Menu(Main main){
+        this.main = main;
+        
+        this.bar = new javafx.scene.control.MenuBar();
+        this.bar.prefWidthProperty().bind(this.main.stage.widthProperty());
+        
+        //main.bp.setTop(this.bar);
+        this.main.layout.setTop(this.bar);
+
+        // File menu - new, save, exit
+        javafx.scene.control.Menu fileMenu = new javafx.scene.control.Menu("File");
+        javafx.scene.control.MenuItem newMenuItem = new javafx.scene.control.MenuItem("New");
+        javafx.scene.control.MenuItem saveMenuItem = new javafx.scene.control.MenuItem("Save");
+        javafx.scene.control.MenuItem exitMenuItem = new javafx.scene.control.MenuItem("Exit");
+        //exitMenuItem.setOnAction(actionEvent -> Platform.exit());
+        
+        javafx.scene.control.Menu toolMenu = new javafx.scene.control.Menu("Tools");
+        javafx.scene.control.MenuItem toggleTools = new javafx.scene.control.MenuItem("Toggle toolbox");
+        //exitMenuItem.setOnAction(actionEvent -> Platfo
+
+        fileMenu.getItems().addAll(newMenuItem, saveMenuItem,
+        new SeparatorMenuItem(), exitMenuItem);
+        toolMenu.getItems().add(toggleTools);
+        this.bar.getMenus().addAll(fileMenu, toolMenu);
+    }
+}
---
+++ src/editor/Style.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package editor;
+
+public class Style {
+    // static fx
+    private static final String background_color_label = "-fx-background-color";
+    private static final String link = ": ";
+    
+    // configurable
+    private static final String background_color = "dddddd";
+    
+    // external useage
+    public String bgColor;
+    
+    public Style(){
+        this.bgColor = Style.background_color_label + Style.link + Style.background_color;
+    }
+
+}
