5c81a7572084c70c535b29da2d50851bbda26ee4
Carlos Beltre <Carlos Beltre>
2016-12-06 01:46:53 -0500

    added the highlight lines code

--- src/UMLEditor/view/Association.java
+++ src/UMLEditor/view/Association.java
@@ -1,21 +1,31 @@
 package UMLEditor.view;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Point2D;
 import javafx.scene.Group;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tooltip;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import javafx.scene.shape.Line;
 import javafx.scene.transform.Rotate;
 
 /**
  * Created by beltre on 12/4/16.
  */
-public class Association extends UmlLine {
+public class Association extends UmlLine implements NodeEditMenu {
 
     private Line minSpreadLine;
     private Line maxSpreadLine;
     final Group group = new Group();
     Rotate rotate = new Rotate();
+    private Images img = new Images();
 
 
     public Association(Anchors a1, Anchors a2)
@@ -106,6 +116,47 @@ public class Association extends UmlLine {
             group.setTranslateY(this.getEndY());
             rotate.setAngle(-slopeInDegrees);
         }
+
+    }
+
+    @Override
+    public void generatePanel(VBox v)
+    {
+        v.getChildren().clear();
+        DropShadow dropShadow = new DropShadow();
+        dropShadow.setHeight(40.0);
+        dropShadow.setWidth(40.0);
+        //dropShadow.setColor(Color.RED);
+
+        Button deleteB = new Button();
+        deleteB.setMaxWidth(Double.MAX_VALUE);
+        VBox.setVgrow(deleteB, Priority.ALWAYS);
+        ImageView deleteBImg = new ImageView(img.getDelete());
+        deleteBImg.setFitHeight(45.0);
+        deleteBImg.setFitWidth(Double.MAX_VALUE);
+        deleteBImg.setPreserveRatio(true);
+        deleteB.setGraphic(deleteBImg);
+        deleteB.setTooltip(new Tooltip("Delete Class"));
+        deleteB.setOnAction((ActionEvent e) -> {
+            deleteSelf();
+            v.getChildren().clear();
+        });
+        deleteB.addEventHandler(MouseEvent.MOUSE_ENTERED,
+                new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent e) {
+                        deleteB.setEffect(dropShadow);
+                    }
+                });
+        deleteB.addEventHandler(MouseEvent.MOUSE_EXITED,
+                new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent e) {
+                        deleteB.setEffect(null);
+                    }
+                });
+
+        v.getChildren().addAll(deleteB);
 
     }
 
--- src/UMLEditor/view/Generalization.java
+++ src/UMLEditor/view/Generalization.java
@@ -1,18 +1,31 @@
 package UMLEditor.view;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Point2D;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.Tooltip;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Polygon;
 import javafx.scene.transform.Rotate;
 
+import javax.xml.soap.Node;
+
 /**
  * Created by beltre on 12/4/16.
  */
-public class Generalization extends UmlLine {
+public class Generalization extends UmlLine implements NodeEditMenu {
 
     private Rotate rotate = new Rotate();
     private Polygon polygon = new Polygon();
+    private Images img = new Images();
 
     public Generalization(Anchors a1, Anchors a2){
         super(a1, a2);
@@ -87,6 +100,47 @@ public class Generalization extends UmlLine {
         pane.getChildren().remove(polygon);
         anchorPoint1.deleteLine(id);
         anchorPoint2.deleteLine(id);
+    }
+
+    @Override
+    public void generatePanel(VBox v)
+    {
+        v.getChildren().clear();
+        DropShadow dropShadow = new DropShadow();
+        dropShadow.setHeight(40.0);
+        dropShadow.setWidth(40.0);
+        //dropShadow.setColor(Color.RED);
+
+        Button deleteB = new Button();
+        deleteB.setMaxWidth(Double.MAX_VALUE);
+        VBox.setVgrow(deleteB, Priority.ALWAYS);
+        ImageView deleteBImg = new ImageView(img.getDelete());
+        deleteBImg.setFitHeight(45.0);
+        deleteBImg.setFitWidth(Double.MAX_VALUE);
+        deleteBImg.setPreserveRatio(true);
+        deleteB.setGraphic(deleteBImg);
+        deleteB.setTooltip(new Tooltip("Delete Class"));
+        deleteB.setOnAction((ActionEvent e) -> {
+            deleteSelf();
+            v.getChildren().clear();
+        });
+        deleteB.addEventHandler(MouseEvent.MOUSE_ENTERED,
+                new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent e) {
+                        deleteB.setEffect(dropShadow);
+                    }
+                });
+        deleteB.addEventHandler(MouseEvent.MOUSE_EXITED,
+                new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent e) {
+                        deleteB.setEffect(null);
+                    }
+                });
+
+        v.getChildren().addAll(deleteB);
+
     }
 
 }
